2e286a6a0288aa36fc877269fbb0c0c4b43d3c7f
tangjinghang <951972210@qq.com>
2020-01-04 15:06:46 +0800

    add main function

--- web.c
+++ web.c
@@ -22,4 +22,5 @@ void get_filetype(char *filename,char *filetype);
 void feed_dynamic(int fd,char *filename,char *cgiargs);
 void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *description);
 
-
+int main()
+{}
