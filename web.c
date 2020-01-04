5187a0c582bcf8a21ba671a64524b4e27b33a9bf
xiao <1306103393@qq.com>
2020-01-04 11:11:59 +0800

    add a web.c

---
+++ web.c
@@ -0,0 +1,23 @@
+#include <stdio.h>  
+#include <sys/socket.h>  
+#include <sys/types.h>  
+#include <netinet/in.h>  
+#include <arpa/inet.h>  
+#include <unistd.h>  
+#include <ctype.h>  
+#include <strings.h>  
+#include <string.h>  
+#include <sys/stat.h>  
+#include <pthread.h>  
+#include <sys/wait.h>  
+#include <stdlib.h>
+
+void proess_trans(int fd);
+void read_requesthdrs(rio_t *rp);
+int is_static(char *url);
+void parse_static_uri(char *uri,char *filename);
+void parse_dynamic_uri(char *uri,char *filename,char *cgiargs);
+void feed_static(int fd,char *filename,int filesize);
+void get_filetype(char *filename,char *filetype);
+void feed_dynamic(int fd,char *filename,char *cgiargs);
+void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *description);
