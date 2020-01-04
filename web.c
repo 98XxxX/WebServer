92007aaa9e5f2a2b0ba105baaa20c2a8f41e31cd
xiao <1306103393@qq.com>
2020-01-04 14:02:05 +0800

    add some code

--- web.c
+++ web.c
@@ -25,8 +25,23 @@ void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *descript
 int main(int argc,char **argv)
 {
 	int listen_sock,conn_sock,port,clientlen;
+	struct sockaddr_in,clientaddr;
 
-
+	if(argc!=2){
+	fprintf(stderr,"usage:%s<port>\n",argv[0]);
+	exit(1);
+	}
+	port = atoi(argv[1]);
+        
+	listen_sock = open_listen_sock(port);
+        while(1)
+	{
+	
+		client = sizeof(clientaddr);
+		conn_sock = accept(listen_sock,(SA *)&clientaddr,&clientlen);
+		process_trans(conn_sock);
+		close(conn_sock);
+	}
 }
 
 
