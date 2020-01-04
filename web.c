afeee486c379f79b44cf006fc86a08869c37267c
tangjinghang <951972210@qq.com>
2020-01-04 14:56:52 +0800

    delete main function

--- web.c
+++ web.c
@@ -22,26 +22,4 @@ void get_filetype(char *filename,char *filetype);
 void feed_dynamic(int fd,char *filename,char *cgiargs);
 void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *description);
 
-int main(int argc,char **argv)
-{
-	int listen_sock,conn_sock,port,clientlen;
-	struct sockaddr_in,clientaddr;
-
-	if(argc!=2){
-	fprintf(stderr,"usage:%s<port>\n",argv[0]);
-	exit(1);
-	}
-	port = atoi(argv[1]);
-        
-	listen_sock = open_listen_sock(port);
-        while(1)
-	{
-	
-		client = sizeof(clientaddr);
-		conn_sock = accept(listen_sock,(SA *)&clientaddr,&clientlen);
-		process_trans(conn_sock);
-		close(conn_sock);
-	}
-}
-
 
